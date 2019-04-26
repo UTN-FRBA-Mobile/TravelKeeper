8a264278324736208e0cc3718bf4d3f0b05d5a32
acapozzoli <acapozzoli@iphoideas.com>
2019-04-26 18:16:51 -0300

    project initial commit

---
+++ app/src/main/java/utn/kotlin/travelkeeper/fragments/MyTripsFragmet.kt
@@ -0,0 +1,20 @@
+package utn.kotlin.travelkeeper.fragments
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MyTripsFragmet : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MyTripsFragmet()
+    }
+}
