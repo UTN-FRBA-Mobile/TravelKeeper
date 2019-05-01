0167ea1c8d7906d53d112ee4c8e767b0dbf6cdfe
Marcos Caccavaio <marcoseze95@hotmail.com>
2019-05-01 00:10:02 -0300

    Agrego Firebase a la aplicacion, permito registrar e iniciar sesion con usuarios a Firebase e integracion de Firebase con Inicio de sesion de google

---
+++ app/src/main/java/utn/kotlin/travelkeeper/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package utn.kotlin.travelkeeper.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
