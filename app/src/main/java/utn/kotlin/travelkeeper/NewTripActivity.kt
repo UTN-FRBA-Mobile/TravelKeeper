7e09ab68d2eb0218eaed49e13ae83a454e56bfb6
Sofía Cortés <sofiavcortes@gmail.com>
2019-05-05 23:25:36 -0300

    Show toast when add new destination

--- app/src/main/java/utn/kotlin/travelkeeper/NewTripActivity.kt
+++ app/src/main/java/utn/kotlin/travelkeeper/NewTripActivity.kt
@@ -2,6 +2,8 @@ package utn.kotlin.travelkeeper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class NewTripActivity : AppCompatActivity() {
 
@@ -10,5 +12,11 @@ class NewTripActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_trip)
 
         this.supportActionBar!!.setTitle(R.string.new_trip_title)
+
+        val button = findViewById<Button>(R.id.button_id)
+        button.setOnClickListener {
+            Toast.makeText(this@NewTripActivity, "Agregar nuevo destino", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
