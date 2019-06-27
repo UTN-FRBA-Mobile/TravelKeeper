527468efc1e7518bbf40fa2c38f83c0d8b8b93e6
Marcos Caccavaio <marcoseze95@hotmail.com>
2019-06-27 00:35:55 -0300

    Agrego editar y eliminar alojamientos

--- app/src/main/java/utn/kotlin/travelkeeper/AccommodationsListActivity.kt
+++ app/src/main/java/utn/kotlin/travelkeeper/AccommodationsListActivity.kt
@@ -1,9 +1,12 @@
 package utn.kotlin.travelkeeper
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -20,7 +23,9 @@ class AccommodationsListActivity : AppCompatActivity() {
     private lateinit var accommodations: MutableList<Accommodation>
     private lateinit var destinationId: String
     private lateinit var tripId: String
+    private lateinit var accommodationSelected: View
     val NEW_ACCOMMODATION_REQUEST = 1
+    val EDIT_ACCOMMODATION_INTENT = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +61,70 @@ class AccommodationsListActivity : AppCompatActivity() {
         })
     }
 
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        accommodationSelected = v!!
+        menuInflater.inflate(R.menu.options_floating_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.edit_option -> {
+                val position = accommodationSelected.tag as Int
+                this.showEditAccommodationActivity(position)
+                return true
+            }
+            R.id.delete_option -> {
+                val position = accommodationSelected.tag as Int
+                this.deleteAccommodation(position)
+                return true
+            }
+            else -> {
+                return super.onContextItemSelected(item)
+            }
+        }
+    }
+
+    private fun showEditAccommodationActivity(position: Int) {
+        val editDestIntent = Intent(this@AccommodationsListActivity, EditAccommodationActivity::class.java)
+        editDestIntent.putExtra("ACCOMMODATION_EDIT", accommodations[position])
+        editDestIntent.putExtra("TRIPID_ACCOMMODATION_EDIT", tripId)
+        editDestIntent.putExtra("DESTID_ACCOMMODATION_EDIT", destinationId)
+        editDestIntent.putExtra("EDIT_ACCOMMODATION_POSITION", position)
+        this@AccommodationsListActivity.startActivityForResult(editDestIntent, EDIT_ACCOMMODATION_INTENT)
+    }
+
+    private fun deleteAccommodation(position: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(R.string.remove_accommodation)
+        builder.setPositiveButton(
+            R.string.log_out_yes
+        ) { dialog, _ ->
+            accommodationService.deleteAccommodation(
+                tripId,
+                destinationId,
+                accommodations[position].id!!,
+                object : AccommodationService.SimpleServiceListener {
+                    override fun onSuccess() {
+                        Toast.makeText(this@AccommodationsListActivity, R.string.accommodation_removed, Toast.LENGTH_SHORT).show()
+                        accommodations.removeAt(position)
+                        resetAdapter()
+                    }
+
+                    override fun onError(exception: Exception) {
+                        Toast.makeText(this@AccommodationsListActivity, R.string.accommodation_not_removed, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            )
+        }
+        builder.setNegativeButton(
+            R.string.log_out_no
+        ) { dialog, _ -> dialog.dismiss() }
+
+        builder.create().show()
+    }
+
     private fun setBackArrow() {
         this.supportActionBar!!.setTitle(R.string.accommodations_list)
         this.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
@@ -88,20 +157,19 @@ class AccommodationsListActivity : AppCompatActivity() {
                 }
             }
         }
-//        else if (requestCode == EDIT_DESTINATION_REQUEST) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                if(data!!.extras != null && data!!.extras.size() > 0) {
-//                    val editDest = data!!.extras["DEST_EDIT"] as TripTimeLineInfo
-//                    val position = data!!.extras["EDIT_DEST_POSITION"] as Int
-//                    if (editDest != null) {
-//                        no_destinations.visibility = View.GONE
-//                        destinations[position] = editDest
-//                        destinations.sortBy { d1 -> d1.start_date }
-//                        resetAdapter()
-//                    }
-//                }
-//            }
-//        }
+        else if (requestCode == EDIT_ACCOMMODATION_INTENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if(data!!.extras != null && data!!.extras.size() > 0) {
+                    val editAccommodation = data!!.extras["ACCOMMODATION_EDIT"] as Accommodation
+                    val position = data!!.extras["ACCOMMODATION_EDIT_DEST_POSITION"] as Int
+                    if (editAccommodation != null) {
+                        accommodations[position] = editAccommodation
+                        accommodations.sortBy { d1 -> d1.startDate }
+                        resetAdapter()
+                    }
+                }
+            }
+        }
     }
 
     private fun resetAdapter() {
---
+++ app/src/main/java/utn/kotlin/travelkeeper/EditAccommodationActivity.kt
@@ -0,0 +1,209 @@
+package utn.kotlin.travelkeeper
+
+import `in`.madapps.placesautocomplete.PlaceAPI
+import `in`.madapps.placesautocomplete.adapter.PlacesAutoCompleteAdapter
+import `in`.madapps.placesautocomplete.model.Place
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.DatePicker
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_edit_accommodation.*
+import utn.kotlin.travelkeeper.DBServices.AccommodationService
+import utn.kotlin.travelkeeper.models.Accommodation
+import utn.kotlin.travelkeeper.models.Address
+import java.text.SimpleDateFormat
+import java.util.*
+
+class EditAccommodationActivity : AppCompatActivity() {
+    private var cal = Calendar.getInstance()
+    private var startDate: Date? = null
+    private var endDate: Date? = null
+    private lateinit var destinationId: String
+    private lateinit var tripId: String
+    private lateinit var accomodation: Accommodation
+    private var position: Int = 0
+
+    val placesApi = PlaceAPI.Builder().apiKey("YOUR_API_KEY").build(this@EditAccommodationActivity)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_accommodation)
+
+        accomodation = intent.extras["ACCOMMODATION_EDIT"] as Accommodation
+        destinationId = intent.getStringExtra("DESTID_ACCOMMODATION_EDIT")
+        tripId = intent.getStringExtra("TRIPID_ACCOMMODATION_EDIT")
+        position = intent.extras["EDIT_ACCOMMODATION_POSITION"] as Int
+
+        setBackArrow()
+        setView()
+        setEditAccommodationButton()
+        setSearchAddresses()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBackArrow() {
+        this.supportActionBar!!.setTitle(R.string.accommodations_list)
+        this.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        this.supportActionBar!!.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun setView() {
+        enter_accommodation_name.setText(accomodation.name)
+        enter_accommodation_address.setText(accomodation.address)
+
+        if(accomodation.reservationCode != null) {
+            enter_accommodation_reservation_number.setText(accomodation.reservationCode)
+        }
+
+        if(accomodation.telephoneNumber != null) {
+            enter_accommodation_telephone_number.setText(accomodation.telephoneNumber)
+        }
+
+        cal.time = accomodation.startDate
+        startDate = accomodation.startDate
+        endDate = accomodation.endDate
+        accommodation_checkin_date_selected!!.text = getDate(accomodation.startDate)
+        accommodation_checkout_date_selected!!.text = getDate(accomodation.endDate)
+        setStartDatePicker()
+        setEndDatePicker()
+    }
+
+    private fun setStartDatePicker() {
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                startDate = cal.time
+                accommodation_checkin_date_selected!!.text = getDate(startDate!!)
+            }
+        }
+
+        accommodation_checkin_date_selected!!.setOnClickListener { view ->
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                // set DatePickerDialog to point to today's date when it loads up
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+    }
+
+    private fun setEndDatePicker() {
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                endDate = cal.time
+                accommodation_checkout_date_selected!!.text = getDate(endDate!!)
+            }
+        }
+
+        accommodation_checkout_date_selected!!.setOnClickListener { view ->
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                // set DatePickerDialog to point to today's date when it loads up
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+    }
+
+    private fun getDate(date: Date): String {
+        val myFormat = "dd/MM/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale("es", "ES"))
+
+        return sdf.format(date)
+    }
+
+    private fun setEditAccommodationButton() {
+        edit_accommodation_button_id.setOnClickListener { _ ->
+            if(isValid()) {
+                val editAccomodation = Accommodation(accomodation.id, enter_accommodation_name.text.toString(), enter_accommodation_address.text.toString(),
+                    startDate!!, endDate!!, enter_accommodation_telephone_number.text.toString(),
+                    enter_accommodation_reservation_number.text.toString())
+                enter_name_error.visibility = View.GONE
+                checkin_date_error.visibility = View.GONE
+                checkout_date_error.visibility = View.GONE
+                editAccommodationInFirebase(editAccomodation)
+            }
+        }
+    }
+
+    private fun setSearchAddresses() {
+        enter_accommodation_address.setAdapter(PlacesAutoCompleteAdapter(this, placesApi))
+        enter_accommodation_address.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, _, position, _ ->
+                val place = parent.getItemAtPosition(position) as Place
+                enter_accommodation_address.setText(place.description)
+            }
+    }
+
+    private fun isValid(): Boolean {
+        var valid = true
+
+        if (enter_accommodation_name.text == null || enter_accommodation_name.text.toString() == "") {
+            enter_name_error.visibility = View.VISIBLE
+            valid = false
+        }
+
+        if(accommodation_checkin_date_selected == null || accommodation_checkin_date_selected.text.toString() == "" || accommodation_checkin_date_selected.text.toString() == "Seleccione una fecha") {
+            checkin_date_error.visibility = View.VISIBLE
+            valid = false
+        }
+
+        if(accommodation_checkout_date_selected == null || accommodation_checkout_date_selected.text.toString() == "" || accommodation_checkout_date_selected.text.toString() == "Seleccione una fecha") {
+            checkout_date_error.visibility = View.VISIBLE
+            valid = false
+        }
+
+        if(startDate != null && endDate != null && endDate!! < startDate!!) {
+            checkout_date_error.setText(R.string.end_date_before_start_date)
+            checkout_date_error.visibility = View.VISIBLE
+            valid = false
+        }
+
+        return valid
+    }
+
+    private fun editAccommodationInFirebase(accommodation: Accommodation) {
+        AccommodationService().editAccommodation(tripId, destinationId, accommodation,
+            object : AccommodationService.CreateAccommodationServiceListener {
+                override fun onSuccess(idCreated: String) {
+                    Toast.makeText(this@EditAccommodationActivity, "Alojamiento editado", Toast.LENGTH_LONG).show()
+
+                    val accommodationIntent = Intent(this@EditAccommodationActivity, AccommodationsListActivity::class.java)
+                    accommodationIntent.putExtra("ACCOMMODATION_EDIT", accommodation)
+                    accommodationIntent.putExtra("ACCOMMODATION_EDIT_DEST_POSITION", position)
+                    setResult(Activity.RESULT_OK, accommodationIntent)
+                    finish()
+                }
+
+                override fun onError(exception: Exception) {
+                    Toast.makeText(this@EditAccommodationActivity, exception.message, Toast.LENGTH_LONG).show()
+                }
+            })
+    }
+}
