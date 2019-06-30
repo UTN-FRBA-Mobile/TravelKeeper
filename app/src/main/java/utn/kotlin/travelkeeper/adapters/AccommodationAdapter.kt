6d9d7d8e676702e29874aeac996b927c24b3d173
Marcos Caccavaio <marcoseze95@hotmail.com>
2019-06-29 23:30:00 -0300

    Cliquear en el alojamiento abre la vista de editar

--- app/src/main/java/utn/kotlin/travelkeeper/adapters/AccommodationAdapter.kt
+++ app/src/main/java/utn/kotlin/travelkeeper/adapters/AccommodationAdapter.kt
@@ -6,10 +6,12 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.accommodation_item_view.view.*
 import utn.kotlin.travelkeeper.AccommodationMapsActivity
+import utn.kotlin.travelkeeper.EditAccommodationActivity
 import utn.kotlin.travelkeeper.R
 import utn.kotlin.travelkeeper.models.Accommodation
 import java.text.SimpleDateFormat
@@ -20,6 +22,8 @@ class AccommodationAdapter (
     private val tripId: String,
     private val destinationId: String) :RecyclerView.Adapter<AccommodationViewHolder>() {
     private lateinit var context: Context
+    val EDIT_ACCOMMODATION_INTENT = 2
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccommodationViewHolder {
         val accommodationItem = LayoutInflater.from(parent.context)
@@ -43,6 +47,17 @@ class AccommodationAdapter (
             intent.putExtra("ACCOMMODATION_LOCATION_LATITUDE",accommodation.latitude)
             intent.putExtra("ACCOMMODATION_LOCATION_LONGITUDE",accommodation.longitude)
             context.startActivity(intent)
+        }
+
+        holder.view.setOnClickListener {
+            val editDestIntent = Intent(context, EditAccommodationActivity::class.java)
+            editDestIntent.putExtra("ACCOMMODATION_EDIT", accommodation)
+            editDestIntent.putExtra("TRIPID_ACCOMMODATION_EDIT", tripId)
+            editDestIntent.putExtra("DESTID_ACCOMMODATION_EDIT", destinationId)
+            editDestIntent.putExtra("EDIT_ACCOMMODATION_POSITION", position)
+
+            val act = context as Activity
+            act.startActivityForResult(editDestIntent, EDIT_ACCOMMODATION_INTENT)
         }
 
         holder.view.tag = position
